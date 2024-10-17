c841e784da7c77409871a52731f8ba0e03201a77
aheit <aeita4@yahoo.com>
2024-10-18 00:09:21 +0300

    fill the request

--- app/src/main/java/com/hamza/ktor/MainActivity.kt
+++ app/src/main/java/com/hamza/ktor/MainActivity.kt
@@ -5,17 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.gson.annotations.SerializedName
 import com.hamza.ktor.ui.theme.KtorExampleTheme
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.gson.gson
 
 class MainActivity : ComponentActivity() {
@@ -39,8 +40,15 @@ fun sendLetter(letter: Letter) {
             gson()
         }
     }
+
+    val response: HttpResponse = client.post("https://jsonplaceholder.typicode.com/posts") {
+        contentType(ContentType.Application.Json)
+        setBody(Letter(1, "I miss you", "How are you?"))
+    }
 }
 
+
+//Template
 data class Letter(
     @SerializedName("UserId") val userId: Int,
     @SerializedName("title") val title: String,

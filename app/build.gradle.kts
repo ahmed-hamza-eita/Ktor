44b803013181e7e12cad19883eaeedde20180db6
aheit <aeita4@yahoo.com>
2024-10-17 23:47:22 +0300

    ktor dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
 
-
+    implementation("io.ktor:ktor-client-core:2.0.0")
+    implementation("io.ktor:ktor-client-cio:2.0.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.0.0")
+    implementation("io.ktor:ktor-serialization-gson:2.0.0")
+    implementation("com.google.code.gson:gson:2.10")
 
 }
